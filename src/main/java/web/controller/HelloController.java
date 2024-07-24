1a39318719de140ab70784a519f68bd74ad1b4fe
aleexqq <shalygin.alexey1@gmail.com>
2024-07-24 16:21:43 +0700

    fixed

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,10 +1,8 @@
 package web.controller;
 
-import model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,22 +19,4 @@ public class HelloController {
 		model.addAttribute("messages", messages);
 		return "index";
 	}
-
-	@GetMapping(value = "cars")
-	public String printCars(@RequestParam(value = "count", required = false) Integer count,
-							ModelMap model) {
-		List<Car> cars = new ArrayList<>();
-		cars.add(new Car(1, "BMW", "black"));
-		cars.add(new Car(2, "Audi", "red"));
-		cars.add(new Car(3, "Ford", "white"));
-		cars.add(new Car(4, "Honda", "yellow"));
-		cars.add(new Car(5, "Mercedes", "green"));
-		if (count != null && count <= cars.size() && count >= 0) {
-			model.addAttribute("cars", cars.subList(0, count));
-		} else {
-			model.addAttribute("cars", cars);
-		}
-		return "cars";
-	}
-	
 }
